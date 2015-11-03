3628c6f746b4e38b69b1e52631626f3bcbf945e0
Chris Troutner <chris.troutner@gmail.com>
2015-11-03 12:14:27 -0500

    Production env set. Port 80 configured.

--- keystone.js
+++ keystone.js
@@ -20,6 +20,7 @@ keystone.init({
 	'favicon': 'public/favicon.ico',
 	'views': 'templates/views',
 	'view engine': 'hbs',
+	'port': '80',
 	
 	'custom engine': handlebars.create({
 		layoutsDir: 'templates/views/layouts',
